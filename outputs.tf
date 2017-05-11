1d916c7566e3d7bd73722ef4aa3ce8fca3e5c5bd
p0bailey <phillip@bailey.st>
2017-05-11 10:46:52 +0100

    Module output remote state

--- outputs.tf
+++ outputs.tf
@@ -6,10 +6,7 @@ output "cidr_block" {
   value = "${var.cidr_block}"
 }
 
-# This boilerplate is needed to expose the vpc ID to terraform remote state.
-#https://www.terraform.io/docs/providers/terraform/d/remote_state.html
+
 output "vpc_id" {
   value = "${aws_vpc.vpc.id}"
 }
-# output "vpc_id" {
-#   value = "${module.vpc.id}"
