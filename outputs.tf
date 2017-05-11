2ad2e93f5918913ba43f2923064b98b763726546
p0bailey <phillip@bailey.st>
2017-05-11 10:13:58 +0100

    Module

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,13 @@ output "public_subnets" {
   value = ["${aws_subnet.public_subnet.*.id}"]
 }
 
-output "vpc_id" {
-  value = "${module.aws_vpc.vpc.id}"
+output "id" {
+  value = "${aws_vpc.vpc.id}"
 }
+
+output "vpc_id" {
+  value = "${module.vpc.id}"
+
 
 output "cidr_block" {
   value = "${var.cidr_block}"
