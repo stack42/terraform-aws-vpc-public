da75180efb6988d983e13b70b6430cf6354c60df
p0bailey <phillip@bailey.st>
2017-05-11 10:27:31 +0100

    Module

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,14 @@ output "public_subnets" {
   value = ["${aws_subnet.public_subnet.*.id}"]
 }
 
-output "id" {
-  value = "${aws_vpc.vpc.id}"
-}
-
-output "vpc_id" {
-  value = "${module.vpc.id}"
-
-
 output "cidr_block" {
   value = "${var.cidr_block}"
 }
+
+# This boilerplate is needed to expose the vpc ID to terraform remote state.
+#https://www.terraform.io/docs/providers/terraform/d/remote_state.html
+output "id" {
+  value = "${aws_vpc.vpc.id}"
+}
+output "vpc_id" {
+  value = "${module.vpc.id}"
